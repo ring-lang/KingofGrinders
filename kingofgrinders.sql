08b35f4ef402ce0ba0ad12da490327481e3631b5
mailmeharry2008 <mailmeharry2008@yahoo.com>
2017-05-28 10:18:02 +0530

    Update kingofgrinders.sql

--- kingofgrinders.sql
+++ kingofgrinders.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Database: `kingofgrinders`
 --
 
+CREATE DATABASE `kingofgrinders`;
+USE  kingofgrinders ;
+
 -- --------------------------------------------------------
 
 --
